04b4dbe9d0876ee162a6e4fce98f18bce198772f
MlMlR <111110044+MlMlR@users.noreply.github.com>
2023-12-01 22:40:15 +0100

    Add new files for Day 2 challenge

---
+++ src/y23/day_02.py
@@ -0,0 +1,13 @@
+file_path = 'src\\y23\\input\\day_02.txt'
+with open(file_path, 'r') as file:
+    data = [line.strip() for line in file.readlines()]
+
+def part1(d):
+    pass
+
+part1(data)
+
+def part2(d):
+    pass
+
+part2(data)
