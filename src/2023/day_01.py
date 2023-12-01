146cd2ec82c9f702e01f9ece12984e15e44aca9a
MlMlR <111110044+MlMlR@users.noreply.github.com>
2023-12-01 21:58:08 +0100

    improve

--- src/2023/day_01.py
+++ src/2023/day_01.py
@@ -2,27 +2,25 @@ file_path = 'input\\2023\\01.txt'
 with open(file_path, 'r') as file:
     data = [line.strip() for line in file.readlines()]
 
-def part1():
-    print(sum(int(x[0] + x[-1]) for l in data if l for x in ([c for c in l if c.isdigit()],)))
+def part1(d):
+    print(sum(int(x[0] + x[-1]) for l in d if l for x in ([c for c in l if c.isdigit()],)))
 
-part1()
+part1(data)
 
-def part2():
-    ans = 0
-    # Process each line in the provided data
-    for line in data:
-        digits = []
-        for i, c in enumerate(line):
+def part2(d):
+    z = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    S = 0
+    for l in d:
+        dg = []
+        for i, c in enumerate(l):
             if c.isdigit():
-                digits.append(c)
-            for d, val in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
-                if line[i:].startswith(val):
-                    digits.append(str(d+1))  # d+1 because enumerate starts at 0
+                dg.append(c)
+            for d, v in enumerate(z):
+                if l[i:].startswith(v):
+                    dg.append(str(d+1))
+        if dg:
+            x = int(dg[0] + dg[-1])
+            S += x
+    print(S)
 
-        # Calculate the score using the first and last digit found
-        if digits:
-            score = int(digits[0] + digits[-1])
-            ans += score
-    print(ans)
-
-part2()
+part2(data)
