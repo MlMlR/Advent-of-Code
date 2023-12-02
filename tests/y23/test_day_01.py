52acdbb0c11a21a028179adf5972cb599bff96de
MlMlR <111110044+MlMlR@users.noreply.github.com>
2023-12-02 20:44:09 +0100

    Update test_day_01.py and test.txt

--- tests/y23/test_day_01.py
+++ tests/y23/test_day_01.py
@@ -3,23 +3,23 @@ from src.y23.day_01 import part1, part2
 
 def test_part1():
     p1 = [
-    "1abc2",
-    "pqr3stu8vwx",
-    "a1b2c3d4e5f",
-    "treb7uchet"
+        "1abc2",
+        "pqr3stu8vwx",
+        "a1b2c3d4e5f",
+        "treb7uchet"
     ]
     res = part1(p1)
-    assert res == 281
+    assert res == 142
 
 def test_part2():
     p2 = [
-    "two1nine",
-    "eightwothree",
-    "abcone2threexyz",
-    "xtwone3four",
-    "4nineeightseven2",
-    "zoneight234",
-    "7pqrstsixteen"
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen"
     ]
     res = part2(p2)
-    assert res == 122
+    assert res == 281
