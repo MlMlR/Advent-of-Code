830d2bb85c5d965765c19cfff4bb36f038d48cd3
MlMlR <111110044+MlMlR@users.noreply.github.com>
2023-12-02 20:44:18 +0100

    Add tests for day 2 and day 3

---
+++ tests/y23/test_day_03.py
@@ -0,0 +1,16 @@
+import pytest
+from src.y23.day_03 import part1, part2
+
+def test_part1():
+    p1 = [
+        1
+    ]
+    res = part1(p1)
+    assert res == True
+
+def test_part2():
+    p2 = [
+        1
+    ]
+    res = part2(p2)
+    assert res == True
