d7eec8813b04a445f19164d54277c41db14a76e5
MlMlR <111110044+MlMlR@users.noreply.github.com>
2023-12-02 08:51:16 +0100

    solve day 2

--- src/y23/day_02.py
+++ src/y23/day_02.py
@@ -2,12 +2,55 @@ file_path = 'src\\y23\\input\\day_02.txt'
 with open(file_path, 'r') as file:
     data = [line.strip() for line in file.readlines()]
 
-def part1(d):
-    pass
+file_path = 'src\\y23\\input\\test.txt'
+with open(file_path, 'r') as file:
+    test_data = [line.strip() for line in file.readlines()]
 
+def part1(d):
+    sum = 0
+    for i, l in enumerate(d):
+        fail = False
+        game, results = l.split(":")
+        results =results.replace("; ", ", ")
+        for result in results.split(", "):
+            result = result.strip()
+            
+            number, color = result.split(" ")
+            if color == "red" and int(number) > 12:
+                fail = True
+            elif color == "green" and int(number) > 13:
+                fail = True
+            elif color == "blue" and int(number) > 14:
+                fail = True
+        if not fail:
+            print(game)
+            sum += i+1
+        fail = False
+
+    print(sum)
+
+print(test_data)
 part1(data)
 
 def part2(d):
-    pass
+    sum = 0
+    for i, l in enumerate(d):
+        r, g, b = 0, 0, 0 
+        game, results = l.split(":")
+        results =results.replace("; ", ", ")
+        for result in results.split(", "):
+            result = result.strip()
+            number, color = result.split(" ")
+            if color == "red":
+                r = max(r, int(number))
+            elif color == "green":
+                g = max(g, int(number))
+            elif color == "blue":
+                b = max(b, int(number))
+        sum += r*g*b
+        r, g, b = 0, 0, 0 
 
+    print(sum)
+
+print(test_data)
 part2(data)
