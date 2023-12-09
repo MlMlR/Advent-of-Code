2b75e7004ff20d1774daecf5385ebefaf5ac5aed
MlMlR <111110044+MlMlR@users.noreply.github.com>
2023-12-09 14:35:50 +0100

    Add files for Day 10 puzzle

---
+++ src/y23/day_10.py
@@ -0,0 +1,20 @@
+with open('.\\src\\y23\\input\\day_10.txt', 'r') as file:
+    data = [line.strip() for line in file.readlines()]
+
+with open('.\\src\\y23\\input\\test.txt', 'r') as file:
+    test_d = [line.strip() for line in file.readlines()]
+
+
+def part1(input):
+    sum = 0
+    return sum
+
+print(part1(test_d))
+#print(part1(data))
+
+def part2(input):
+    sum = 0
+    return sum
+
+print(part2(test_d))
+#print(part2(data))
