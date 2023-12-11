95cf02b2a8ed08f2e107ab0b0f614112663e1de7
MlMlR <111110044+MlMlR@users.noreply.github.com>
2023-12-11 10:43:51 +0100

    Refactor code to improve readability and performance

--- src/y23/day_11.py
+++ src/y23/day_11.py
@@ -1,8 +1,12 @@
 with open('.\\src\\y23\\input\\day_11.txt', 'r') as file:
-    data = [list(line.strip()) for line in file.readlines()]
+    lines = [line.strip() for line in file.readlines()]
+    grid = [list(line.strip()) for line in file.readlines()]
+
 
 with open('.\\src\\y23\\input\\test.txt', 'r') as file:
-    test_d = [list(line.strip()) for line in file.readlines()]
+    t_lines = [line.strip() for line in file.readlines()]
+    t_grid = [list(line.strip()) for line in file.readlines()]
+
 
 
 
@@ -10,19 +14,35 @@ with open('.\\src\\y23\\input\\test.txt', 'r') as file:
 
 def part1(input):
     sum = 0
-    empty_rows = [r for r, row in enumerate(input) if all(cell != "#" for cell in row)]
-    empty_cols = [c for c, col in enumerate(zip(*input)) if all(cell != "#" for cell in col)]
-    points = [(r, c) for r, row in enumerate(input) for c, cell in enumerate(row) if cell == "#"]
+    empty_rows = []
+    empty_cols = []
+    points = []
 
+    for r, row in enumerate(input):
+        if all(val != "#" for val in row):
+            empty_rows.append(r)
+
+    for c, col in enumerate(zip(*input)):
+        if all(cell != "#" for cell in col):
+            empty_cols.append(c)
+
+    for r, row in enumerate(input):
+        for c, cell in enumerate(row):
+            if cell == "#":
+                points.append((r,c))
 
     for i, (r1, c1) in enumerate(points):
         for r2, c2 in points[:i]:  
-            # Add scaled value if row is empty, otherwise add 1
             for r in range(min(r1, r2), max(r1, r2)):
-                sum += 2 if r in empty_rows else 1
-            # Add scaled value if column is empty, otherwise add 1
+                if r in empty_rows:
+                    sum += 2 
+                else:
+                    sum += 1
             for c in range(min(c1, c2), max(c1, c2)):
-                sum += 2 if c in empty_cols else 1
+                if r in empty_cols:
+                    sum += 2 
+                else:
+                    sum += 1
 
     return sum
 
@@ -39,10 +59,8 @@ def part2(input):
 
     for i, (r1, c1) in enumerate(points):
         for r2, c2 in points[:i]:  
-            # Add scaled value if row is empty, otherwise add 1
             for r in range(min(r1, r2), max(r1, r2)):
                 sum += 1000000 if r in empty_rows else 1
-            # Add scaled value if column is empty, otherwise add 1
             for c in range(min(c1, c2), max(c1, c2)):
                 sum += 1000000 if c in empty_cols else 1
 
